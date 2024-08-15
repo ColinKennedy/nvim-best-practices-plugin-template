4fd1e954177b4c971b11b46a19fa2690bf895c04
Colin Kennedy <colinvfx@gmail.com>
2024-08-14 23:13:41 -0700

    Working on an argparse rework

--- lua/plugin_name/_cli/argparse.lua
+++ lua/plugin_name/_cli/argparse.lua
@@ -14,151 +14,151 @@ local _ArgumentType = {
   positional = "positional",
 }
 
-local function is_char(character)
+local _State = {
+  argument_start = "argument_start",
+  normal = "normal",
+  in_flag = "in_flag",
+  in_quote = "in_quote",
+  in_value = "in_value",
+}
+
+local function is_alpha_numeric(character)
   return character:match('[^=\'"%s]') ~= nil
+end
+
+local function _is_whitespace(next)
+  return next:match("%s")
+end
+
+local function _is_quote(character)
+  return character == '"' or character == "'"
 end
 
 -- TODO: Change the variable names. They're awful
 
-local function _parse_args(x)
+local function _parse_args(whole_text)
   --- @type string[], table<string,string|boolean>
-  local pos_args, named_args = {}, {}
+  local position_arguments, named_arguments = {}, {}
 
-  local last_argument = nil
-  local state = 'in_arg'
-  local cur_arg = ''
-  local cur_val = ''
-  local cur_quote = ''
+  local state = _State.argument_start
+  local current_argument = ''
+  local current_name = ''
+  local is_escaping = false
+  local needs_name = false
+  local needs_value = false
 
-  local function peek(idx)
-    return x:sub(idx + 1, idx + 1)
+  local function peek(index)
+    return whole_text:sub(index + 1, index + 1)
   end
 
-  local i = 1
-  while i <= #x do
-    local ch = x:sub(i, i)
-
-    if state == 'in_arg' then
-      if is_char(ch) then
-        if ch == '-' and peek(i) == '-' then
-          state = 'in_flag'
-          cur_arg = ''
-          i = i + 1
-        else
-          cur_arg = cur_arg .. ch
-        end
-      elseif ch:match('%s') then
-        local position = #pos_args + 1
-        last_argument = {
-          argument_type = _ArgumentType.positional .. "b",
-          data = position,
-        }
-        pos_args[position] = cur_arg
-        state = 'in_ws'
-      elseif ch == '=' then
-        cur_val = ''
-        local next_ch = peek(i)
-        if next_ch == "'" or next_ch == '"' then
-          cur_quote = next_ch
-          i = i + 1
-          state = 'in_quote'
-        else
-          state = 'in_value'
-        end
-      end
-    elseif state == 'in_flag' then
-      if ch:match('%s') then
-        last_argument = {
-          argument_type = _ArgumentType.named,
-          data = cur_arg,
-        }
-        named_args[cur_arg] = true
-        state = 'in_ws'
-      else
-        cur_arg = cur_arg .. ch
-      end
-    elseif state == 'in_ws' then
-      if is_char(ch) then
-        if ch == '-' and peek(i) == '-' then
-          state = 'in_flag'
-          cur_arg = ''
-          i = i + 1
-        else
-          state = 'in_arg'
-          cur_arg = ch
-        end
-      end
-    elseif state == 'in_value' then
-      if is_char(ch) then
-        cur_val = cur_val .. ch
-      elseif ch:match('%s') then
-        last_argument = {
-          argument_type = _ArgumentType.named,
-          data = cur_arg,
-        }
-        named_args[cur_arg] = cur_val
-        cur_arg = ''
-        state = 'in_ws'
-      end
-    elseif state == 'in_quote' then
-      local next_ch = peek(i)
-      if ch == '\\' and next_ch == cur_quote then
-        cur_val = cur_val .. next_ch
-        i = i + 1
-      elseif ch == cur_quote then
-        last_argument = {
-          argument_type = _ArgumentType.named,
-          data = cur_arg,
-        }
-        named_args[cur_arg] = cur_val
-        state = 'in_ws'
-        if next_ch ~= '' and not next_ch:match('%s') then
-          error('malformed argument: ' .. next_ch)
-        end
-      else
-        cur_val = cur_val .. ch
-      end
-    end
-    i = i + 1
-  end
-
-  if #cur_arg > 0 then
-    if state == 'in_arg' then
-      local position = #pos_args + 1
-      last_argument = {
-        argument_type = _ArgumentType.positional .. "z",
-        data = position,
-      }
-      pos_args[position] = cur_arg
-    elseif state == 'in_flag' then
-      last_argument = {
-        argument_type = _ArgumentType.named,
-        data = cur_arg,
-      }
-      named_args[cur_arg] = true
-    elseif state == 'in_value' then
-      last_argument = {
-        argument_type = _ArgumentType.named,
-        data = cur_arg,
-      }
-      named_args[cur_arg] = cur_val
+  local function _add_to_output()
+    -- We reached the end of the quote
+    if needs_value then
+      named_arguments[current_name] = current_argument
+    else
+      table.insert(position_arguments, current_argument)
     end
   end
 
-  -- TODO: Remove
-  -- print('DEBUGPRINT[1]: argparse.lua:148: last_argument=' .. vim.inspect(last_argument))
-  return pos_args, named_args, last_argument
+  local function _reset_argument()
+    current_argument = ""
+  end
+
+  local function _reset_all()
+    _reset_argument()
+    current_name = ""
+    is_escaping = false
+    needs_name = false
+    needs_value = false
+    state = _State.argument_start
+  end
+
+  local index = 1
+
+  while index <= #whole_text do
+    local character = whole_text:sub(index, index)
+
+    local function _append_to_wip_argument(alternate_character)
+      current_argument = current_argument .. (alternate_character or character)
+    end
+
+    if character == "\\" then
+      is_escaping = not is_escaping
+    end
+
+    if state == _State.argument_start then
+      if is_alpha_numeric(character) then
+        if character == '-' and peek(index) == '-' then
+          state = _State.in_flag
+          _reset_argument()
+          index = index + 1
+          needs_name = true
+          needs_value = true
+        elseif _is_quote(character) then
+          state = _State.in_quote
+          needs_value = false
+        else
+          state = _State.normal
+          _append_to_wip_argument()
+          needs_value = false
+        end
+      end
+    elseif state == _State.in_quote then
+      if not is_escaping and _is_quote(character) then
+        _add_to_output()
+        _reset_all()
+      else
+        _append_to_wip_argument()
+      end
+    elseif state == _State.in_flag then
+      if character == "=" then
+        needs_name = false
+        current_name = current_argument
+        _reset_argument()
+
+        if _is_quote(peek(index)) then
+          state = _State.in_quote
+          index = index + 1
+        else
+          state = _State.normal
+        end
+      elseif _is_whitespace(character) then
+        current_name = current_argument
+        current_argument = true
+        _add_to_output()
+        _reset_all()
+      elseif needs_name then
+        _append_to_wip_argument()
+      end
+    elseif state == _State.normal then
+      if is_escaping then
+        local next = peek(index)
+        _append_to_wip_argument(next)
+      elseif _is_whitespace(character) then
+        _add_to_output()
+        _reset_all()
+      else
+        _append_to_wip_argument()
+      end
+    end
+
+    index = index + 1
+  end
+
+  if state == _State.normal and current_argument ~= "" then
+    _add_to_output()
+  end
+
+  return {position_arguments, named_arguments}
 end
 
 --- Get all positional arguments and named arguments.
 ---
---- @param x string
+--- @param whole_text string
 --- @return string[], table<string,string|boolean>
 ---
-function M.parse_args(x)
-  local positional_arguments, named_arguments, _ = _parse_args(x)
-
-  return {positional_arguments, named_arguments}
+function M.parse_args(whole_text)
+  return _parse_args(whole_text)
 end
 
 return M
