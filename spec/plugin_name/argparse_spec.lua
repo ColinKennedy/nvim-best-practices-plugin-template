f70311d480d2f585a03682f9d25ffb28275d35d5
Colin Kennedy <colinvfx@gmail.com>
2024-08-14 23:17:36 -0700

    Fixed unittest order

--- spec/plugin_name/argparse_spec.lua
+++ spec/plugin_name/argparse_spec.lua
@@ -3,29 +3,29 @@ local argparse = require("plugin_name._cli.argparse")
 describe("default", function()
   it("works even if #empty #simple", function()
     print('DEBUGPRINT[2]: argparse_spec.lua:5: argparse.parse_args("")=' .. vim.inspect(argparse.parse_args("")))
-    assert.same(argparse.parse_args(""), {{}, {}})
+    assert.same({{}, {}}, argparse.parse_args(""))
   end)
 end)
 
 describe("positional arguments", function()
   it("#simple single argument", function()
-    assert.same(argparse.parse_args("foo"), {{"foo"}, {}})
+    assert.same({{"foo"}, {}}, argparse.parse_args("foo"))
   end)
 
   it("#simple multiple arguments", function()
-    assert.same(argparse.parse_args("foo bar"), {{"foo", "bar"}, {}})
+    assert.same({{"foo", "bar"}, {}}, argparse.parse_args("foo bar"))
   end)
 
   it("#escaped positional arguments 001", function()
-    assert.same(argparse.parse_args("\\ foo"), {{" foo"}, {}})
+    assert.same({{" foo"}, {}}, argparse.parse_args("\\ foo"))
   end)
 
   it("#escaped positional arguments 002", function()
-    assert.same(argparse.parse_args("foo\\ "), {{"foo "}, {}})
+    assert.same({{"foo "}, {}}, argparse.parse_args("foo\\ "))
   end)
 
   it("#escaped positional arguments 003", function()
-    assert.same(argparse.parse_args("foo\\ bar"), {{"foo bar"}, {}})
+    assert.same({{"foo bar"}, {}}, argparse.parse_args("foo\\ bar"))
   end)
 end)
 
@@ -38,35 +38,39 @@ describe("quotes", function()
   end)
 
   it("#multiple arguments", function()
-    assert.same(argparse.parse_args("foo bar"), {{"foo bar"}, {}})
+    assert.same({{"foo bar"}, {}}, argparse.parse_args("foo bar"))
   end)
 
   it("#escaped spaces 001", function()
-    assert.same(argparse.parse_args("\\ foo"), {{" foo"}, {}})
+    assert.same({{" foo"}, {}}, argparse.parse_args("\\ foo"))
   end)
 
   it("#escaped spaces 002", function()
-    assert.same(argparse.parse_args("foo\\ "), {{"foo "}, {}})
+    assert.same({{"foo "}, {}}, argparse.parse_args("foo\\ "))
   end)
 
   it("#escaped spaces 003", function()
-    assert.same(argparse.parse_args("foo\\ bar"), {{"foo bar"}, {}})
+    assert.same({{"foo bar"}, {}}, argparse.parse_args("foo\\ bar"))
   end)
 end)
 
 describe("double-dash flags", function()
   it("mixed --flag", function()
     assert.same(
-      argparse.parse_args("--foo-bar --fizz"), {{}, {["foo-bar"] = true, fizz=true}}
+      {{}, {["foo-bar"] = true, fizz=true}},
+      argparse.parse_args("--foo-bar --fizz")
     )
   end)
 
   it("single --flag", function()
-    assert.same(argparse.parse_args("--foo-bar"), {{}, {["foo-bar"] = true}})
-    assert.same(argparse.parse_args("--foo"), {{}, {foo=true}})
+    assert.same({{}, {["foo-bar"] = true}}, argparse.parse_args("--foo-bar"))
+    assert.same({{}, {foo=true}}, argparse.parse_args("--foo"))
   end)
 
   it("multiple", function()
-    assert.same(argparse.parse_args("--foo --bar --fizz --buzz"), {{}, {foo=true, bar=true, fizz=true, buzz=true}})
+    assert.same(
+      {{}, {foo=true, bar=true, fizz=true, buzz=true}},
+      argparse.parse_args("--foo --bar --fizz --buzz")
+    )
   end)
 end)
