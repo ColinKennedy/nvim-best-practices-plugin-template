a62e8062c9d9ccf2467efb6cf294fec57aaf2f0e
Colin Kennedy <colinvfx@gmail.com>
2024-11-25 13:44:40 -0800

    Fixed llscheck issues

--- spec/test_utilities/mock_test.lua
+++ spec/test_utilities/mock_test.lua
@@ -62,7 +62,7 @@ end
 
 --- Stop loggers from sending to stdout / stderr.
 function M.silence_loggers()
-    local high_level = 99999999999
+    local high_level = "fatal"
     logging._DEFAULTS.level = high_level
 
     for _, logger in pairs(logging._LOGGERS) do
