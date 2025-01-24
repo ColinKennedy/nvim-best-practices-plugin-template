4c99b75bda4ba36fb46b84b99651b07bfcf1c658
Colin Kennedy <colinvfx@gmail.com>
2025-01-24 08:25:48 -0800

    Removed lua/ and spec/

--- lua/plugin_template/_commands/hello_world/say/constant.lua
+++
@@ -1,9 +0,0 @@
---- Symbolic variables to use for all `hello-world say`-related commands.
----
----@module 'plugin_template._commands.hello_world.say.constant'
----
-
-return {
-    Subcommand = { phrase = "phrase", word = "word" },
-    Keyword = { style = { lowercase = "lowercase", uppercase = "uppercase" } },
-}
