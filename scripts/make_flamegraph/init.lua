1906cf2d74e66eab083f4f1346bbb5dc308d591c
Colin Kennedy <colinvfx@gmail.com>
2025-01-26 13:34:39 -0800

    Added plugins

---
+++ scripts/make_flamegraph/init.lua
@@ -0,0 +1,14 @@
+-- TODO: Replace with relative paths once it's confirmed working
+-- local logging = "/home/selecaoone/repositories/personal/.config/nvim/bundle/nvim-best-practices-plugin-template/.dependencies/mega.logging/lua"
+local root =
+    "/home/selecaoone/repositories/personal/.config/nvim/bundle/nvim-best-practices-plugin-template/.dependencies/mega.busted/lua"
+-- package.path = logging .. "/?.lua;" .. package.path
+package.path = root .. "/?.lua;" .. root .. "/?/init.lua;" .. package.path
+
+-- print(package.searchpath('mega.logging', package.path))
+-- print(package.searchpath('pl', package.path))
+-- error('stop')
+
+-- TODO: check for dependencies first
+
+require("mega.busted.make_profile").main()
--- spec/test_utilities/mock_test.lua
+++ spec/test_utilities/mock_test.lua
@@ -3,10 +3,14 @@
 ---@module 'test_utilities.mock_test'
 ---
 
+local logging = require("mega.logging")
+
 local M = {}
 
-local _ORIGINAL_INSPECT = vim.inspect
 local _DATA = nil
+local _ORIGINAL_DEFAULT_LEVEL
+local _ORIGINAL_INSPECT = vim.inspect
+local _ORIGINAL_LEVELS = {}
 
 --- Temporarily track vim.inspect calls.
 ---
@@ -36,6 +40,34 @@ end
 function M.silence_all_internal_prints()
     ---@diagnostic disable-next-line: duplicate-set-field
     vim.notify = function(...) end -- luacheck: ignore 212
+end
+
+--- Revert the current `logging` back to the last `save_logger`.
+function M.reset_loggers()
+    logging._DEFAULTS.level = _ORIGINAL_DEFAULT_LEVEL
+
+    for _, logger in pairs(logging._LOGGERS) do
+        logger.level = _ORIGINAL_LEVELS[logger.name]
+    end
+end
+
+--- Keep track of the current `logging` so it can be `reset_loggers` later.
+function M.save_loggers()
+    _ORIGINAL_DEFAULT_LEVEL = logging._DEFAULTS.level
+
+    for _, logger in pairs(logging._LOGGERS) do
+        _ORIGINAL_LEVELS[logger.name] = logger.level
+    end
+end
+
+--- Stop loggers from sending to stdout / stderr.
+function M.silence_loggers()
+    local high_level = "fatal"
+    logging._DEFAULTS.level = high_level
+
+    for _, logger in pairs(logging._LOGGERS) do
+        logger.level = high_level
+    end
 end
 
 return M
