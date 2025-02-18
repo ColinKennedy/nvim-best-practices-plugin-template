45df2f4c43ac34ec1ff280e0f0359e2d6ee75427
Colin Kennedy <colinvfx@gmail.com>
2025-02-18 12:18:42 -0800

    Added standalone_test.lua

---
+++ scripts/make_flamegraph/standalone_test.lua
@@ -0,0 +1,22 @@
+-- TODO: Replace with relative paths once it's confirmed working
+local logging = "/home/selecaoone/repositories/personal/.config/nvim/bundle/nvim-best-practices-plugin-template/.dependencies/mega.logging/lua"
+
+local root =
+    "/home/selecaoone/repositories/personal/.config/nvim/bundle/nvim-best-practices-plugin-template/.dependencies/mega.busted/lua"
+package.path = logging .. "/?.lua;" .. package.path
+package.path = root .. "/?.lua;" .. root .. "/?/init.lua;" .. package.path
+
+-- print(package.searchpath('mega.logging', package.path))
+-- print(package.searchpath('pl', package.path))
+-- error('stop')
+
+-- TODO: check for dependencies first
+
+---@type string[]
+local arguments = {}
+
+for _, item in ipairs(arg or {}) do
+    table.insert(arguments, item)
+end
+
+require("mega.busted.make_standalone_profile").main(arguments)
